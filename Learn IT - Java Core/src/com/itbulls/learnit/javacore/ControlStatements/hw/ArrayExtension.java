8622e85a73850404518430dcc7c68d11edead895
Greg <greg2025@hotmail.com>
2023-07-05 13:18:06 -0400

    added GCD and Array Extension .java files

--- Learn IT - Java Core/src/com/itbulls/learnit/javacore/ControlStatements/hw/ArrayExtension.java
+++ Learn IT - Java Core/src/com/itbulls/learnit/javacore/ControlStatements/hw/ArrayExtension.java
@@ -5,35 +5,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ArrayExtension {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Please, enter length of initial array: ");
-		int baseArrayLength = sc.nextInt();
-		int[] arr = generateRandomArray(baseArrayLength);
-		int[] extendedArray = extendArray(arr);
-		System.out.println("*** Initial array ***");
-		System.out.println(Arrays.toString(arr));
-		System.out.println("*** Extended array ***");
-		System.out.println(Arrays.toString(extendedArray));
-	}
 
-	/**
-	 * The method extends array.
-	 * If array {1, 2, 3} has been passed to this method then array {1, 2, 3, 2, 4, 6}
-	 * is returned from this method.
-	 * 
-	 * @param arr - base of extension. Extended array contains elements from this array
-	 * and additionally contains the same elements multiplied by two. 
-	 * @return extended array.
-	 */
-	public static int[] extendArray(int[] arr) {
-//	    <write your code here>
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the length of the initial array: ");
+        int baseArrayLength = sc.nextInt();
+        int[] arr = generateRandomArray(baseArrayLength);
+        int[] extendedArray = extendArray(arr);
+        System.out.println("*** Initial array ***");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("*** Extended array ***");
+        System.out.println(Arrays.toString(extendedArray));
+    }
 
-	public static int[] generateRandomArray(int amountOfElements) {
-//		<write your code here>
-	}
+    // Method to extend the array by appending elements multiplied by two
+    public static int[] extendArray(int[] arr) {
+        int extendedLength = arr.length * 2; // Calculate the length of the extended array
+        int[] extendedArray = new int[extendedLength]; // Create a new array with the calculated length
 
+        for (int i = 0; i < arr.length; i++) {
+            extendedArray[i] = arr[i]; // Copy elements from the original array to the extended array
+            extendedArray[arr.length + i] = arr[i] * 2; // Set corresponding element in the extended array to the element multiplied by two
+        }
+
+        return extendedArray; // Return the extended array
+    }
+
+    // Method to generate a random array with the specified length
+    public static int[] generateRandomArray(int amountOfElements) {
+        int[] randomArray = new int[amountOfElements]; // Create a new array with the specified length
+        Random random = new Random();
+
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = random.nextInt(100) + 1; // Generates a random number in the range from 1 to 100
+        }
+
+        return randomArray; // Return the randomly generated array
+    }
 }
-
---
+++ Learn IT - Java Core/src/com/itbulls/learnit/javacore/ControlStatements/hw/GreatestCommonDivisor.java
@@ -0,0 +1,34 @@
+package com.itbulls.learnit.javacore.ControlStatements.hw;
+
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter two numbers separated by space: ");
+        String userInput = sc.nextLine();
+        
+        String[] inputArgumentsArray = userInput.split("\\s+");
+        int number1 = Integer.parseInt(inputArgumentsArray[0]);
+        int number2 = Integer.parseInt(inputArgumentsArray[1]);
+        
+        System.out.println(gcdRecursive(number1, number2));
+    }
+    
+    public static int gcdRecursive(int firstNumber, int secondNumber) {
+        // Take absolute values of the input numbers to ensure positive values for GCD calculation
+        firstNumber = Math.abs(firstNumber);
+        secondNumber = Math.abs(secondNumber);
+
+        // Base case: If the second number is zero, the GCD is the first number
+        if (secondNumber == 0) {
+            return firstNumber;
+        }
+
+        // Recursive call: Calculate GCD using Euclidean algorithm
+        return gcdRecursive(secondNumber, firstNumber % secondNumber);
+    }
+
+}
+
